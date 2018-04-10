2dd6a90a9fd32adff81055699f7d57dd9ce203a6
Modou Lo <modoulo99@icloud.com>
2018-04-10 08:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function getFirstSelector(ul) {
   return document.querySelector(ul);
   
 }
+
+function nestedTarget() {
+  
+  return document.getElementById("nested").querySelector(".target")
+  
+}
