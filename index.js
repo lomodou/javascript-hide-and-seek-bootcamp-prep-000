4fcb6d36d69905f886647c1f0d1d1a8406c97539
Modou Lo <modoulo99@icloud.com>
2018-04-10 08:11:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ function increaseRankBy(n) {
   }
   
 }
+
+function deepestChild() {
+  
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  
+  for(let i=0;i<lis.length-1;i++) {
+    
+    test=lis[i].querySelector("div");
+    
+  }
+  
+  return test;
+
+}
