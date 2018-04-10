575f1c6d69f62aab896d6a2f4bcb69be910bd701
Modou Lo <modoulo99@icloud.com>
2018-04-10 08:13:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function getFirstSelector(ul) {
+  
+  return document.querySelector(ul);
+  
+}
+
+function nestedTarget() {
+  
+  return document.getElementById("nested").querySelector(".target")
+  
+}
+
+function increaseRankBy(n) {
+  
+  var list = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  
+  for(let i = 0; i < list.length; i++) {
+    
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+    
+  }
+  
+}
+
+function deepestChild() {
+  
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  
+  for(let i=0;i<lis.length-1;i++) {
+    
+    test=lis[i].querySelector("div");
+    
+  }
+  
+  return test;
+
+}
